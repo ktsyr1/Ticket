f6cd888b8402524613667d8b346cfe6e4afb218f
Qotayba Mohammad <46694587+ktsyr1@users.noreply.github.com>
2023-09-16 23:39:02 +0000

    1

--- src/pages/page/[slug].jsx
+++ src/pages/page/[slug].jsx
@@ -25,9 +25,9 @@ export default function PageOne({ data }) {
             {/* content */}
             <SEO title={` ${data?.title}`} description={data?.bio} image={data?.image} />
             <div className="bord col page p-10">
-                <img src={data?.image || "/images/logo-full.png"} alt={`صورة ${data.title}`} className="" style={{ width: '320px', borderRadius: '20px', boxShadow: '0 0 10px #ddd' }} loading="lazy" />
+                <img src={data?.image || "/images/logo-full.png"} alt={`صورة ${data?.title}`} className="" style={{ width: '320px', borderRadius: '20px', boxShadow: '0 0 10px #ddd' }} loading="lazy" />
 
-                <h1 className="m-0">{data.title}</h1>
+                <h1 className="m-0">{data?.title}</h1>
                 <div className="box row m-10">
                     <p className="m-0">{new Date(data.create_at)}</p>
                     <p className="m-0">{data?.cat?.map(a => <Link href={`/page?cat=${a}`} >{a}</Link>)}</p>
